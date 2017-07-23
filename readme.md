383981f0a7cd81fb16ebf0bdaa6f06a25ee94829
duendeinexistente <duendeinexistente@gmail.com>
2017-07-23 17:59:48 -0300

    Modified wait.lua to not use all CPU power available, adding lua-socket as a requirement in the process. Old, lua-only sleep function commented out.

--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 Scripts made for my own convenience as I learn how to use lua.
 
-Wait: Waits a presset amount of time. Usage: wait [time]
+Wait: Waits a presset amount of time. 
+
+	**Usage**: wait [time]
+
+	**Requires**: lua-socket.
--- wait.lua
+++ wait.lua
@@ -1,9 +1,16 @@
 #!/usr/bin/lua
+-- Disabled because, while it didn't require any library outside the default lua package, it used absurd amounts of CPU.
 
--- We define the sleep(time) function, which I really don't understand not being part of Lua's default functions.
-function sleep(s)
-  local ntime = os.clock() + s
-  repeat until os.clock() > ntime
+---- We define the sleep(time) function, which I really don't understand not being part of Lua's default functions.
+-- function sleep(s)
+--  local ntime = os.clock() + s
+--  repeat until os.clock() > ntime
+--end
+
+
+socket = require("socket")
+function sleep(sec)
+    socket.sleep(sec)
 end
 
 -- Now we set the argument to a variable
